2761ec298e00c493c63922d6650d9493a4cab1d4
Jackson Gee <jacksongee2005@gmail.com>
2026-06-12 22:06:52 +0800

    version 1 of model training

--- src/train.py
+++ src/train.py
@@ -1,7 +1,26 @@
 from clean import load_data, clean_data
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
 df = load_data()
 df_clean = clean_data(df)
 
 X = df_clean.drop("price", axis = 1)
 y = df_clean["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+rmse = np.sqrt(mse)
+
+print("MAE:", mae)
+print("RMSE:", rmse)
+print("R²:", r2)
